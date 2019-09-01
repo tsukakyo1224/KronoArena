9c50dbe53950e9c89cadc7e75f42ba279fbbf7c5
tsukakyo1224 <kyon-1224.rioru@ezweb.ne.jp>
2019-09-02 02:50:50 +0900

    Update TimerScript.cs

--- KronoArena/Assets/Scripts/TimerScript.cs
+++ KronoArena/Assets/Scripts/TimerScript.cs
@@ -20,6 +20,8 @@ public class TimerScript : MonoBehaviour
     //秒数増えるカウント
     public static int Count;
 
+    public PhotonView photonView;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -34,6 +36,8 @@ public class TimerScript : MonoBehaviour
 
         //秒数増えるカウント
         Count = 0;
+
+        photonView = GetComponent<PhotonView>();
 
     }
 
@@ -66,11 +70,23 @@ public class TimerScript : MonoBehaviour
             if (TotalTime < 0.0f)
             {
                 GameObject.Find("TurnCol").GetComponent<TurnCol>().ChangeTurn();
+                photonView.RPC("ScondUp", PhotonTargets.All);
                 //TotalTime += 10.0f;
                 Debug.Log("TimeTurnChange");
             }
         }
 
+    }
+
+
+    [PunRPC]
+    public void SecondUp()
+    {
+        if((PhotonNetwork.player.ID == 1 && TurnCol.P1_Turn == true) ||
+            (PhotonNetwork.player.ID == 2 && TurnCol.P2_Turn == true))
+        {
+            TotalTime += 3.0f;
+        }
     }
 
     void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)

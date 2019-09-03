a5bfa6984d01b254f7d1a704a66c61fc79119952
tsukakyo1224 <kyon-1224.rioru@ezweb.ne.jp>
2019-09-03 16:41:31 +0900

    Update Knight_Data.cs

--- KronoArena/Assets/Scripts/CharaData/Knight_Data.cs
+++ KronoArena/Assets/Scripts/CharaData/Knight_Data.cs
@@ -205,6 +205,7 @@ public class Knight_Data : MonoBehaviour
     {
         if (num == 1)
         {
+            Debug.Log(this + "スキル1ため");
             animator.SetBool("Skill1_Trigger", false);
             animator.SetBool("Skill1", true);
             var instantiateEffect = GameObject.Instantiate(Skill1_Set, this.transform.position, Quaternion.identity) as GameObject;
@@ -218,6 +219,7 @@ public class Knight_Data : MonoBehaviour
 
         else if (num == 2)
         {
+            Debug.Log(this + "スキル1発動");
             animator.SetBool("Skill1", false);
             animator.SetBool("Skill1_Trigger", true);
             var instantiateEffect = GameObject.Instantiate(Skill1, this.transform.position, Quaternion.identity) as GameObject;

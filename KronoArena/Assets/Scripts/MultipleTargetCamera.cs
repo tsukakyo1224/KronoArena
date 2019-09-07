20a5580ede1ff6ddfaf6500c7e5eeff412000043
tsukakyo1224 <kyon-1224.rioru@ezweb.ne.jp>
2019-09-07 10:53:27 +0900

    Update MultipleTargetCamera.cs

--- KronoArena/Assets/Scripts/MultipleTargetCamera.cs
+++ KronoArena/Assets/Scripts/MultipleTargetCamera.cs
@@ -66,7 +66,7 @@ public class MultipleTargetCamera : MonoBehaviour
             maxcount = count;
             count = 0;
             pos += pos2;
-            center = pos / count;
+            center = pos / maxcount;
             this.transform.LookAt(center);          //CenterPointのポジションを中心に配置
             Debug.Log(center);
             //cameraPos.LookAt(this.transform);           //CameraPositionを中心の方向に向かせる

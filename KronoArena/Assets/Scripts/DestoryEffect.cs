ec6d2aa099b58afe7d6490a33327c9da37c4ef19
tsukakyo1224 <kyon-1224.rioru@ezweb.ne.jp>
2019-09-08 15:37:55 +0900

    attack2

--- KronoArena/Assets/Scripts/DestoryEffect.cs
+++ KronoArena/Assets/Scripts/DestoryEffect.cs
@@ -134,7 +134,7 @@ public class DestoryEffect : MonoBehaviour {
                     float random = Random.Range(0.9f, 1.1f);    //ランダム関数
                     float damage;   //ダメージ量
                                     //ダメージを与える
-                    damage = (this.GetComponent<Status>().Magic_Attack / ((1 + obj.GetComponent<Status>().Defense) / 7.5f));
+                    damage = (this.GetComponent<Status>().Magic_Attack / ((1 + obj.GetComponent<Status>().Defense) / 10));
                     damage *= random;
                     obj.GetComponent<Status>().HP -= (int)damage;
                     //表示

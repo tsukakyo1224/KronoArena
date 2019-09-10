4cc7d0efe054ac129b1d50df09499190852bfceb
tsukakyo1224 <kyon-1224.rioru@ezweb.ne.jp>
2019-09-10 10:53:30 +0900

    animbug

---
+++ KronoArena/Assets/Scripts/AnimDestroy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimDestroy : MonoBehaviour
+{
+    private float DesTime = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DesTime += Time.deltaTime;
+        if(DesTime >= 1.0f)
+        {
+            this.GetComponent<Animator>().enabled = false;
+        }
+    }
+}
